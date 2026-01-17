2be656da92595e072048e3999bba81d4a6d90ba2
Nicolas Panetti <panettinico@gmail.com>
2026-01-17 16:09:36 +0100

    Aggiornamento Riferimenti

---
+++ riferimenti utili ai DB.py
@@ -0,0 +1,15 @@
+"""
+Autonoleggio --> Lab06
+
+Musei Torino --> Lab07
+
+Gestione Energia --> Lab08
+
+Tour --> Lab09
+
+Hub e Tratte --> Lab10
+
+Rifugi e Sentieri --> Lab11 e Lab12
+
+Geni e Cromosomi -->Lab13
+"""
