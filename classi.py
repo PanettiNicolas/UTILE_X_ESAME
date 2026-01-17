e94e203d3c9f349ad3507c21b8bd0b33fce64f0d
Nicolas Panetti <panettinico@gmail.com>
2026-01-17 12:28:23 +0100

    Aggiornamento MVC

---
+++ classi.py
@@ -0,0 +1,23 @@
+@dataclass()
+class Impianto:
+    id: int
+    nome: str
+    indirizzo: str
+
+    # RELAZIONI (1:N)
+    lista_consumi: list = None
+
+    def get_consumi(self):
+        """ Aggiorna e Restituisce la lista di consumi (self.lista_consumi) associati all'impianto"""
+        if self.lista_consumi is None:
+            self.lista_consumi = ConsumoDAO.get_consumi(self.id)
+        return self.lista_consumi
+
+    def __eq__(self, other):
+        return isinstance(other, Impianto) and self.id == other.id
+
+    def __str__(self):
+        return f"{self.id} | {self.nome} | Indirizzo: {self.indirizzo}"
+
+    def __repr__(self):
+        return f"{self.id} | {self.nome} | Indirizzo: {self.indirizzo}"
--- mvc.py
+++ mvc.py
@@ -1,23 +1,120 @@
-@dataclass()
-class Impianto:
-    id: int
-    nome: str
-    indirizzo: str
+"""
+-----------------Struttura MVC + DAO----------------
 
-    # RELAZIONI (1:N)
-    lista_consumi: list = None
+>MODEL: logica e dati
 
-    def get_consumi(self):
-        """ Aggiorna e Restituisce la lista di consumi (self.lista_consumi) associati all'impianto"""
-        if self.lista_consumi is None:
-            self.lista_consumi = ConsumoDAO.get_consumi(self.id)
-        return self.lista_consumi
+        Funzioni: - Rappresenta la struttura dati
+                  - Si occupa di gestire lo stato dell'applicazione
+                  - Interagisce con il database (DAO)
 
-    def __eq__(self, other):
-        return isinstance(other, Impianto) and self.id == other.id
+        Collegato a DAO e Controller
 
-    def __str__(self):
-        return f"{self.id} | {self.nome} | Indirizzo: {self.indirizzo}"
+>VIEW: l'interfaccia con Flet
 
-    def __repr__(self):
-        return f"{self.id} | {self.nome} | Indirizzo: {self.indirizzo}"
+        Funzioni: - Rappresenta l'interfaccia utente (UI)
+                  - Riceve i dati elaboorati dal Model attraverso il Controller
+
+        Collegato a Controller
+
+>CONTROLLER: l'intermediario
+
+             Funzioni: - Funziona da intermediario tra Model e View
+                       - Gestisce la logica del flusso dell'applicazione:
+                         riceve gli input dalla View, li convalida, interroga il Model e aggiorna la View con i risultati
+
+            Collegato a Model e View
+
+
+>DAO: è il responsabile esclusivo della persistenza dei dati.
+
+     Funzione: - Preleva i dati dal DB, traducendo rihge di tabelle SQL in oggetti Python
+
+     Collegato a Model
+
+     !!! Il DAO non viene MAI chiamato direttamente dalla View o dal Controller MA solo dal Model
+
+
+Riassunto comunicazione componenti:
+
+Database <-> DAO <-> Model <-> Controller <-> View
+
+
+"""
+
+#-------------Struttura standard INTERROGAZIONE DAO--------------
+
+from database.DB_connect import DBConnect
+from model.connessione import Conessione
+
+class DAO:
+    @staticmethod
+    def get_dati_filtrati(parametro_filtro):
+        cnx = DBConnect.get_connection()
+        cursor = cnx.cursor(dictionary=True)
+        result = [] / {}       #In base a se il risultato sia una lista o un dizionario
+        
+        if cnx is None:         #Verifica connessione
+            print("Errore di connessione al database")
+            return None
+        
+        query = """ query con filtro in %s """
+        
+        try:                    #Verifica riuscita query
+            cursor.execute(query, (parametro_filtro,))
+            
+            for row in cursor:
+                result.append(row)
+                
+        except Exception as e:
+            print(f"Errore durante lo svolgimento della query: {e}")
+            
+        finally:
+            cursor.close()
+            cnx.close()
+            
+        return result
+        
+
+
+#--------------Struttura standard MODEL-------------
+
+from database.dao import DAO
+import networkx as nx
+
+class Model:
+    def __init__(self):
+        #Attrributi model UTILIZZATI all'intterno della classe
+        #Funzioni load_ che carichino gli attributi al Model prelevati dal DB
+        #!!! Gli attributi utilizzati SOLO nel Controller possono essere passati direttamente attraverso le funzioni get_
+        self.lista_attributi = []
+
+    def get_attributi(self):
+        return DAO.get_attributi()         #<--- Nel Controller
+
+    def load_attributi(self):
+        self.lista_attributi = DAO.get_attributi()       #<--- Nel Model
+
+    def crea_grafo(self):
+        #Vedi file su Grafi
+        pass
+
+    def ricorsione(self):
+        #Vedi file su Ricorsione
+        pass
+
+
+#---------------Struttura standard CONTROLLER--------------
+
+class Controller:
+    def __init__(self):
+        #Riferimenti ai componenti MVC
+        self._view = view
+        self.model = model
+
+        #Eventuali attributi del controller
+
+
+    def handle_bottone(self, e):
+        #Vedi file su Flet
+        pass
+
