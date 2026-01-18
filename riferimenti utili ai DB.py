81621e9b4ed2867bd62187aa6543d82d0566d7c3
Nicolas Panetti <panettinico@gmail.com>
2026-01-18 10:28:13 +0100

    Aggiornamento Flet

--- riferimenti utili ai DB.py
+++ riferimenti utili ai DB.py
@@ -1,4 +1,6 @@
 """
+-------------LABORATORI------------
+
 Autonoleggio --> Lab06
 
 Musei Torino --> Lab07
@@ -12,5 +14,27 @@ Hub e Tratte --> Lab10
 Rifugi e Sentieri --> Lab11 e Lab12
 
 Geni e Cromosomi -->Lab13
+
+--------------IN CLASSE-------------
+
+Dizionario --> Week08
+
+Meteo --> Week09
+
+Metro Parigi --> Week10 - Week12 - Week13
+
+Artsmia --> Week11 - Week13
+
+Voli --> Week13
+
+----------SIMULAZIONI ESAME---------
+
+iTunes --> SE iTunes
+
+Baseball --> SE Baseball
+
+Avvistamenti Ufo --> SE Ufo
+
+Bike Store --> SE Bike Store
 """
 
